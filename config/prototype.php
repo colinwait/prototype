7a80952f0eb44c2d68c83f38d2a2eab6f88299d8
卢杨 <luyang@hoge.cn>
2018-01-29 14:42:21 +0800

    增加云服务分类

--- config/prototype.php
+++ config/prototype.php
@@ -3,7 +3,8 @@ return [
     'categories' => [
         'mxu'     => 'MXU-1.2系列',
         'new-mxu' => 'MXU-1.3系列',
-        'new-m2o' => 'NEW-M2O系列'
+        'new-m2o' => 'NEW-M2O系列',
+        'cloud'   => '云服务系列',
     ],
     'path'       => env('PROTOTYPE_PATH'),
     'host'       => env('PROTOTYPE_HOST'),
